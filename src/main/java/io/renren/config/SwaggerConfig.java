2fdbb82a1f8fad0c6c23b953df92a8d227da2782
Mark <sunlightcs@gmail.com>
2018-06-24 22:12:38 +0800

    v2.1.0

--- src/main/java/io/renren/config/SwaggerConfig.java
+++ src/main/java/io/renren/config/SwaggerConfig.java
@@ -9,9 +9,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -24,15 +29,19 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/static/swagger/");
     }
 
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
             .apiInfo(apiInfo())
             .select()
-            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))           //加了ApiOperation注解的方法，生成接口文档
-            //.apis(RequestHandlerSelectors.basePackage("io.renren.modules.job.controller"))  //包下的类，生成接口文档
+            //加了ApiOperation注解的类，才生成接口文档
+            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+            //包下的类，才生成接口文档
+            //.apis(RequestHandlerSelectors.basePackage("io.renren.controller"))
             .paths(PathSelectors.any())
-            .build();
+            .build()
+            .securitySchemes(security());
     }
 
     private ApiInfo apiInfo() {
@@ -40,8 +49,14 @@ public class SwaggerConfig implements WebMvcConfigurer {
             .title("人人开源")
             .description("renren-fast文档")
             .termsOfServiceUrl("http://www.renren.io")
-            .version("2.0")
+            .version("2.1")
             .build();
+    }
+
+    private List<ApiKey> security() {
+        return newArrayList(
+            new ApiKey("token", "token", "header")
+        );
     }
 
 }
